853106a87c8d4d9335e9e45bec440411b88f135c
Rahim51044 <mdarahim17@gmail.com>
2025-07-14 17:53:22 +0600

    pagination complete

--- Desktop/Projects/JS REACT/milestone-12/online-forum-server/index.js
+++ Desktop/Projects/JS REACT/milestone-12/online-forum-server/index.js
@@ -309,7 +309,7 @@ async function run() {
       res.send(result);
     });
 
-    // ✅ Add comments
+    // ✅ Add comment
     app.post("/comments", async (req, res) => {
       const comment = req.body; // postId, text, userEmail, userName, time
       const result = await commentsCollection.insertOne(comment);
